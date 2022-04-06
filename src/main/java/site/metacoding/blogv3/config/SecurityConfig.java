0f159874712efdaaa0224a39c96a2f4d67cdbea2
AR <aki1013@naver.com>
2022-04-06 16:50:54 +0900

    시큐리티 인증 필터 설정

---
+++ src/main/java/site/metacoding/blogv3/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package site.metacoding.blogv3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 걸어줘야 해당 파일로 시큐리티가 활성화 된다.
+@Configuration // Ioc에 등록 , 등록이 됐다고 해서 시큐리티가 작동하는것은 아니다
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 인증 설정하는 메서드
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // super.configure(http);
+        http.csrf().disable(); // 이거 안하면 postman테스트 못함.
+        http.authorizeRequests()
+                .antMatchers("/s/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm")
+                .defaultSuccessUrl("/");
+    }
+
+}
